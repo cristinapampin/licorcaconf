3f2d54c3a296437b1a58c458281e28cdec71b772
Cris <cristina.rpampin@gmail.com>
2022-06-08 13:09:51 +0200

    call4papers WIP

---
+++ src/app/model/typePaper.enum.ts
@@ -0,0 +1,4 @@
+export enum typePaper {
+  talk,
+  openSpace
+}
--- src/app/pages/call4papers/call4papers.component.ts
+++ src/app/pages/call4papers/call4papers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { typePaper } from 'src/app/model/typePaper.enum';
 
 @Component({
   selector: 'app-call4papers',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Call4papersComponent implements OnInit {
 
-  constructor() { }
+  paperForm: FormGroup
+  typePaper: typePaper
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createPaperform()
   }
 
+  createPaperform() {
+    this.paperForm = this.formBuilder.group({
+      typePaper: ['', Validators.required],
+      titlePaper: ['', Validators.required],
+      descriptionPaper: ['', [Validators.required, Validators.minLength(350)]],
+      topicPaper: ['', Validators.required],
+      firstTalk: [''],
+      needMentoring: [''],
+      commentsPaper: [''],
+      speakerFullName: ['', Validators.required],
+      speakerMail: ['', [Validators.required, Validators.email]],
+      speakerBio: ['', [Validators.required, Validators.minLength(250)]],
+      speakerTwitter: [''],
+      speakerPronoun: ['', Validators.required],
+      speakerRole: ['', Validators.required],
+      speakerSeniority: ['', Validators.required],
+      isDoublePaper: [''],
+      speakerNeedsDoubleBed: [''],
+      speakerLocation: ['', Validators.required],
+      commentsSpeaker: [''],
+    })
+  }
+
+  savePaper() {
+    console.log(this.paperForm.value, this.paperForm.valid)
+  }
 }

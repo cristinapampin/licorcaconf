8d2d021754692d974d7c673605fdf6e3f2c24c91
Cris <cristina.rpampin@gmail.com>
2022-06-08 13:09:36 +0200

    update conduct-code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatDividerModule } from '@angular/material/divider'
-import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './pages/home/home.component';
 import { ConductCodeComponent } from './pages/conduct-code/conduct-code.component';
 import { Call4papersComponent } from './pages/call4papers/call4papers.component';
@@ -34,7 +39,12 @@ import { LicorcaTeamComponent } from './pages/licorca-team/licorca-team.componen
     MatMenuModule,
     MatSidenavModule,
     FlexLayoutModule,
-    MatDividerModule
+    MatDividerModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
